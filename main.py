06f0b44aa29d77cd0ddb115d5027d32912803d7e
Thebloodshadow <corentin.diet@gmail.com>
2021-03-17 18:56:26 +0100

    Just changed some useless things and added lisivility to the code. Note that right and left may be inverted

--- main.py
+++ main.py
@@ -9,29 +9,29 @@ from microbit import *
 
 display.off()
 
-def commande_mot1(direction, speed):
+def command_mot1(direction, speed):
     pin8.write_digital(direction)
     pin1.write_analog(speed)
 
-def commande_mot2(direction, speed):
+def command_mot2(direction, speed):
     pin12.write_digital(direction)
     pin2.write_analog(speed)
 
 def forward():
-    commande_mot1(0, 512)
-    commande_mot2(1, 512)
+    command_mot1(0, 512)
+    command_mot2(1, 512)
 
 def stop():
-    commande_mot1(0, 0)
-    commande_mot2(1, 0)
+    command_mot1(0, 0)
+    command_mot2(1, 0)
 
 def left():
-    commande_mot1(0, 512) #I don't know if this is really left since i don't know in wich order are the motors
-    commande_mot2(1, 0)   #You may need to change left and right if your robot is not rotating in the direction it should
+    command_mot1(0, 512) #I don't know if this is really left since i don't know in wich order are the motors
+    command_mot2(1, 0)   #You may need to change left and right if your robot is not rotating in the direction it should
 
 def right():              #I don't know if this is really right since i don't know in wich order are the motors
-    commande_mot1(0, 0)   #You may need to change left and right if your robot is not rotating in the direction it should
-    commande_mot2(1, 512)
+    command_mot1(0, 0)   #You may need to change left and right if your robot is not rotating in the direction it should
+    command_mot2(1, 512)
 
 while True:
 
