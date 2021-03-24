dbfe857033c269485aa2bc5daa0730afb558f766
Thebloodshadow <corentin.diet@gmail.com>
2021-03-24 07:06:26 +0100

    Using motorbit for test and making the catapult launch

--- main.py
+++ main.py
@@ -4,7 +4,8 @@
 # NOTICE : Some value has to be #
 #   edited before utilisation   #
 #################################
-
+seconds_launch = 0
+sseconds = 0
 from microbit import *
 
 display.off()
@@ -18,31 +19,50 @@ def command_mot2(direction, speed):
     pin2.write_analog(speed)
 
 def forward():
-    command_mot1(0, 512)
-    command_mot2(1, 512)
+    command_mot1(0, 1023)
+    command_mot2(1, 1023)
 
 def stop():
     command_mot1(0, 0)
     command_mot2(1, 0)
 
 def left():
-    command_mot1(0, 512) #I don't know if this is really left since i don't know in wich order are the motors
-    command_mot2(1, 0)   #You may need to change left and right if your robot is not rotating in the direction it should
+    command_mot1(0, 1023)
+    command_mot2(1, 0)
 
-def right():              #I don't know if this is really right since i don't know in wich order are the motors
-    command_mot1(0, 0)   #You may need to change left and right if your robot is not rotating in the direction it should
-    command_mot2(1, 512)
+def right():
+    command_mot1(0, 0)
+    command_mot2(1, 1023)
+
+def catapult_launch():
+    stop() 
+    global seconds_launch
+    if seconds_launch < 2:
+        seconds_launch += 1
+        basic.pause(1000)
+    else:
+         pin3.write_digital(1)
 
 while True:
-
-    if pin7.read_digital() and pin6.read_digital():
+    seconds += 1
+    if seconds == 5:
+        catapult_launch()
+    elif pin7.read_digital() and pin6.read_digital():
         forward()
-
     elif pin6.read_digital():
         left()
-
     elif pin7.read_digital():
         right()
-
     else:
       stop()
+    basic.pause(1000)
+
+#Test to see something
+
+motorbit.forward(100)
+basic.pause(2000)
+motorbit.back(100)
+basic.pause(2000)
+motorbit.right(100)
+basic.pause(2000)
+motorbit.left(100)
