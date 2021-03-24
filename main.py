9a18d19dce123d70fd08043b12292481877edcd6
Thebloodshadow <corentin.diet@gmail.com>
2021-03-24 07:33:04 +0100

    Rewireted some of the code to work plus added a test to use motorbit

--- main.py
+++ main.py
@@ -2,46 +2,60 @@
 # This code was made by Vortezz #
 # and edited by Thebloodshadow  #
 # NOTICE : Some value has to be #
-#   edited before utilisation   #
+#   edited before use           #
 #################################
-seconds_launch = 0
-sseconds = 0
 from microbit import *
+from basic import pause
+from motorbit import forward as forward_test
+from motorbit import back as back_test
+from motorbit import right as left_test
+from motorbit import left as right_test
+
+seconds_launch = 0
+seconds = 0
 
 display.off()
+
 
 def command_mot1(direction, speed):
     pin8.write_digital(direction)
     pin1.write_analog(speed)
 
+
 def command_mot2(direction, speed):
     pin12.write_digital(direction)
     pin2.write_analog(speed)
+
 
 def forward():
     command_mot1(0, 1023)
     command_mot2(1, 1023)
 
+
 def stop():
     command_mot1(0, 0)
     command_mot2(1, 0)
+
 
 def left():
     command_mot1(0, 1023)
     command_mot2(1, 0)
 
+
 def right():
     command_mot1(0, 0)
     command_mot2(1, 1023)
 
-def catapult_launch():
-    stop() 
+
+def catapult_launch(): # Will send a pulse to your catpult after a certain amount of time
+    stop()
     global seconds_launch
     if seconds_launch < 2:
         seconds_launch += 1
-        basic.pause(1000)
+        pause(1000)
     else:
-         pin3.write_digital(1)
+        pin3.write_digital(1)
+
 
 while True:
     seconds += 1
@@ -54,15 +68,15 @@ while True:
     elif pin7.read_digital():
         right()
     else:
-      stop()
-    basic.pause(1000)
+        stop()
+    pause(1000)
 
-#Test to see something
+# Test to see something (Motorbit may be more simple than sending a pulse to a pin)
 
-motorbit.forward(100)
-basic.pause(2000)
-motorbit.back(100)
-basic.pause(2000)
-motorbit.right(100)
-basic.pause(2000)
-motorbit.left(100)
+forward_test(100)
+pause(2000)
+back_test(100)
+pause(2000)
+right_test(100)
+pause(2000)
+left_test(100)
